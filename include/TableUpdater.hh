751582dfe8214acea8d67cdfb5256a07a68697e7
veloxid <bachmair@phys.ethz.ch>
2014-10-02 15:43:28 +0200

    makde it compile on linux

--- include/TableUpdater.hh
+++ include/TableUpdater.hh
@@ -4,5 +4,7 @@
 #include <stdlib.h> 
 #include <unistd.h>
 #include <fcntl.h>           /* Definition of AT_* constants */
+#include <stdio.h>
+#include <string.h>
 int WriteTableFile(std::string fname, std::map<std::string, std::string> *m);
 int ReadTableFile(std::string fname, std::map<std::string, std::string> *m);
